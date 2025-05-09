17704a16282cc65204c9e63c465bf63a70410034
Gaurav Upreti <garvitupreti2002@gmail.com>
2025-05-09 19:19:00 +0530

    Made connection between Frontend and Backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import http from 'http';
+import {Server} from 'socket.io';
 
 const app = express();
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors:{
+        origin: "*",
+    },
+});
+
+io.on("connection",(socket)=>{
+    console.log("Client Connected", socket.id);
+});
 
 const port = process.env.PORT || 5000;
 
