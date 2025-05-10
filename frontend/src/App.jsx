e989c0c94b8b5a8bc18b0558568d6b0cecfa8857
Gaurav Upreti <garvitupreti2002@gmail.com>
2025-05-10 12:23:41 +0530

    Completed the basic layout designing by use CSS.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './App.css';
-import io from 'socket.io-client'
+import io from 'socket.io-client';
+import Editor from '@monaco-editor/react';
+
 
 const socket = io("http://localhost:5000")
 
@@ -8,14 +10,21 @@ const App = () => {
   const [joined, setJoined] = useState(false);
   const [roomId,setRoomId] = useState("");
   const [userName,setUserName] = useState("");
+  const [language,setLanguage] = useState("python");
+  const [code,setCode] = useState("");
 
   const joinRoom = () => {
     if(roomId && userName){
       socket.emit("join",{roomId,userName})
       setJoined(true);
-      
     }
   }
+
+  const copyRoomId = () => {};
+
+  const handleCodeChange = (newCode) => {
+    setCode(newCode);
+  };
 
   if(!joined){
     return (
@@ -31,7 +40,41 @@ const App = () => {
   }
 
   return (
-    <div>User Joined</div>
+    <div className='editor-container'>
+      <div className="sidebar">
+        <div className="room-info">
+          <h2>Code Room ID: {roomId}</h2>
+          <button onClick={copyRoomId} className='copy-button'>Copy ID</button>
+        </div>
+        <h3>Current Users in Room:</h3>
+        <ul>
+          <li>Demo-1</li>
+          <li>Demo-2</li>
+        </ul>
+        <p className='typing-indicator'>User typing.....</p>
+        <select className='language-selector'>
+          <option value="javascript">JavaScript</option>
+          <option value="python">Python</option>
+          <option value="java">Java</option>
+          <option value="cpp">C++</option>
+        </select>
+        <button className='leave-button'>Leave Room</button>
+      </div>
+
+      <div className="editor-wrapper">
+        <Editor
+          height={"100%"}
+          language={language}
+          value={code}
+          onChange={handleCodeChange}
+          theme="vs-dark"
+          options={{
+            minimap: { enabled: false },
+            fontSize: 14,
+          }}
+        />
+      </div>
+    </div>
   )
 }
 
